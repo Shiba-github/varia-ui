4e4faa287384132fbedab420e5402022a9d7cfec
shiba <septem.one71@gmail.com>
2023-04-16 22:42:44 +0900

    fix yarn lock

--- src/style/mainTheme/extendHtmlStyles/index.ts
+++ src/style/mainTheme/extendHtmlStyles/index.ts
@@ -1,9 +1,8 @@
-import type { StyleFunctionProps } from '@chakra-ui/styled-system'
 import { body } from './body'
 import { h1 } from './h1'
 
 // 特定のhtml要素に対してスタイルをつけることができる
-export const globalTheme = (props: StyleFunctionProps) => ({
+export const globalTheme = () => ({
     body:body,
     h1:h1
 })
